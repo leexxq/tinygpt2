f1f3a7c32d3a84c2015b069db1167f7110f778b1
Xie Xiaoqi <2734648380@qq.com>
2026-04-14 11:28:24 +0800

    update

--- train_gpt2.py
+++ train_gpt2.py
@@ -4,18 +4,50 @@ from typing import Any
 
 import torch
 import torch.nn as nn 
+        
 from torch.nn import functional as F
 
 import math
 
 import tiktoken
+class DataLoaderLIte:
+    def __init__(self,B,T) :
+        self.B = B
+        self.T = T
+        with open('data/shakespeare/input.txt','r') as f:
+            text = f.read()
+
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens)//(B*T)} batches")
+        self.current_position = 0
+
+    def next_batch(self):
+        B,T = self.B,self.T
+        buf = self.tokens[self.current_position:self.current_position+B*T+1]
+        x = (buf[:-1]).view(B,T)
+        y = (buf[1:]).view(B,T)
+
+        self.current_position += B*T
+
+        if self.current_position + (B*T + 1) > len(self.tokens): 
+            self.current_position = 0
+
+        return x,y
+
+
+
 
 class CausalSelfAttention(nn.Module):
     def __init__(self, config:GPTConfig):
         super().__init__()
         assert config.n_embd % config.n_head == 0
         self.c_attn = nn.Linear(config.n_embd,3*config.n_embd)
-        self.c_proj =  nn.Linear(config.n_embd,config.n_embd)
+        self.c_proj = nn.Linear(config.n_embd,config.n_embd)
+        self.c_proj.register_buffer('scale_init',None,persistent=False)
 
         self.n_embd = config.n_embd
         self.n_head = config.n_head
@@ -27,25 +59,22 @@ class CausalSelfAttention(nn.Module):
         qkv : torch.Tensor = self.c_attn(x)
         q,k,v = qkv.split(self.n_embd,dim = 2)
         k = k.view(B,T,self.n_head,C//self.n_head).transpose(1,2) #(B,nh,T,hs)
-        q = k.view(B,T,self.n_head,C//self.n_head).transpose(1,2) #(B,nh,T,hs)
-        v = k.view(B,T,self.n_head,C//self.n_head).transpose(1,2) #(B,nh,T,hs)
+        q = q.view(B,T,self.n_head,C//self.n_head).transpose(1,2) #(B,nh,T,hs)
+        v = v.view(B,T,self.n_head,C//self.n_head).transpose(1,2) #(B,nh,T,hs)
 
-        att = (q @ k.transpose(-2,-1)) / math.sqrt(k.size(-1)) #(B,nh,T,T)
+        # attention 
+        # att = (q @ k.transpose(-2,-1)) / math.sqrt(k.size(-1)) #(B,nh,T,T)
+        # att = att.masked_fill(self.get_buffer('bias')[:,:,:T,:T] == 0,float('-inf'))
+        # att = F.softmax(att,dim=-1)
+        # y = att @ v #(B,nh,T,T) @ (B,nh,T,ns) -> (B,nh,T,ns)
+        # flash attention
+        y = F.scaled_dot_product_attention(q,k,v,is_causal=True)
 
-        att = att.masked_fill(self.get_buffer('bias')[:,:,:T,:T] == 0,float('-inf'))
 
-        
-        att = F.softmax(att,dim=-1)
-
-        y = att @ v #(B,nh,T,T) @ (B,nh,T,ns) -> (B,nh,T,ns)
         y = y.transpose(1,2).contiguous().view(B,T,C) # (B,nh,T,ns) -> (B,T,nh,ns) -> (B,T,C)
         y = self.c_proj(y)
         
         return y
-
-
-
-
 
 
 class MLP(nn.Module):
@@ -54,11 +83,13 @@ class MLP(nn.Module):
         self.c_fc =  nn.Linear(config.n_embd,4 * config.n_embd)
         self.gelu = nn.GELU(approximate='tanh')
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.register_buffer('scale_init',None,persistent=False)
 
     def forward(self,x):
-        self.c_fc(x)
-        self.gelu(x)
-        self.c_proj(x)
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
 
 class Block(nn.Module):
     def __init__(self,config:GPTConfig):
@@ -71,15 +102,18 @@ class Block(nn.Module):
     def forward(self,x):
         x = x + self.attn(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
+        return x
 
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int =6
-    n_head: int = 6
-    n_embd: int = 354
+    block_size: int = 1024
+    vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
+    # dropout: float = 0.0
+    # bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
 
 class GPT(nn.Module):
     def __init__(self,config:GPTConfig):
@@ -91,27 +125,58 @@ class GPT(nn.Module):
                 h = nn.ModuleList(Block(config) for _ in range(config.n_layer) ),
                 ln_f = nn.LayerNorm(config.n_embd),
             ))
-        self.lm_head = nn.Linear(config.n_embd,config.vocab_size ,bias=True)
+        self.lm_head = nn.Linear(config.n_embd,config.vocab_size ,bias=False)
 
-    def forward(self,idx:torch.Tensor):
+        assert type(self.transformer.wte) == nn.Embedding
+        self.transformer.wte.weight = self.lm_head.weight 
+        self.apply(self.__init_weights)
+
+    def __init_weights(self,module):
+        if isinstance(module,nn.Linear):
+            std = 0.02
+            if hasattr(module,'scale_init'):
+                std *= (2 * self.config.n_layer)** -0.5
+            torch.nn.init.normal_(module.weight,mean=0.0,std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+
+        elif isinstance(module,nn.Embedding):
+            torch.nn.init.normal_(module.weight,mean=0.0,std=0.02)
+
+
+    def configure_optimizer(self,weight_decay,learning_rate,device) -> torch.optim.AdamW:
+        optimizer = torch.optim.AdamW(model.parameters(),lr=3e-4,betas=(0.9,0.95),eps=1e-8)
+        return optimizer
+
+
+    def forward(self,idx:torch.Tensor,targets = None):
         B,T = idx.size()
         assert T <= self.config.block_size , f"connot forward sequence of lenth{T},block size is {self.config.block_size}"
 
-        pos = torch.range(0,T,dtype= torch.long,device=idx.device)
+        pos = torch.arange(0,T,dtype= torch.long,device=idx.device)
 
-        pos_emb = self.transformer.wpe(pos)
-        tok_emb = self.transformer.wte(idx)
+        assert type(self.transformer.wpe) == nn.Embedding
+        assert type(self.transformer.wte) == nn.Embedding
+        pos_emb = self.transformer.wpe(pos) 
+        tok_emb = self.transformer.wte(idx) 
 
         x = pos_emb + tok_emb
 
+        assert type(self.transformer.h) == nn.ModuleList
         for block in self.transformer.h:
             x = block(x)
 
+        assert type(self.transformer.ln_f) == nn.LayerNorm
         x = self.transformer.ln_f(x)
 
-        logits = self.lm_head(x)
+        logits = self.lm_head(x) #(B,T,vocab_size)
+        #logits.view(-1,logits.size(-1)) : (B*T,vocab_size)
+        loss = None 
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1,logits.size(-1)),targets.view(-1))
+            pass
 
-        return logits
+        return logits, loss
 
     @classmethod
     def from_pretrained(cls, model_type, override_args=None):
@@ -171,13 +236,38 @@ class GPT(nn.Module):
         return model 
 
 
+max_lr = 6e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_step = 50
 
+max_batch_size = 16 * 256 
+B = 8
+T = 256
+grad_accum_steps= max_batch_size//(B*T)
 
 num_return_sequences = 5
 max_length = 30
 
-torch.manual_seed(42)
-torch.cuda.manual_seed(42)
+def get_lr(iter):
+    if iter < warmup_steps: 
+        return max_lr * (iter + 1) /warmup_steps
+
+    if iter > max_step:
+        return max_lr
+    
+    decay_ratio = (iter - warmup_steps) /(max_step - warmup_steps)
+    assert 0 <= decay_ratio <=1
+    coeff = 0.5 * (1.0 + math.cos(math.pi*decay_ratio))
+    return min_lr + coeff*(max_lr - min_lr)
+
+
+
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1337)
+if torch.mps.is_available():
+    torch.mps.manual_seed(1337)
 
 device = "cpu"
 if torch.cuda.is_available():
@@ -187,31 +277,42 @@ elif hasattr(torch.backends,"mps") and torch.backends.mps.is_available():
 
 print("using device:",device)
 
+train_loader = DataLoaderLIte(B=B,T=T)
 
-with open('data/shakespeare/input.txt','r') as f:
-    text = f.read()
+# torch.set_float32_matmul_precision('medium')
 
-data = text[:1000]
-
-enc = tiktoken.get_encoding('gpt2')
-tokens = enc.encode(data)
-B,T = 4, 32
-buf = torch.tensor(tokens[:B*T+1])
-x = buf[:-1].view(B,T)
-y = buf[1:].view(B,T)
-
-
-model = GPT(GPTConfig())
+model = GPT(GPTConfig(vocab_size=50304))
 model.to(device=device)
-logits:torch.Tensor = model(x)
+# model = torch.compile(model)
 
-print(logits.shape)
+# optimizer = torch.optim.AdamW(model.parameters(),lr=3e-4,betas=(0.9,0.95),eps=1e-8)
+optimizer = model.configure_optimizer(weight_decay=0.1,learning_rate=6e-4,device=device)
 
+import time
+for step in range(max_step):
+    t0 = time.time()
+    optimizer.zero_grad()
 
+    loss_accum = 0.0
+    for micro_step in range(grad_accum_steps):
+        x,y = train_loader.next_batch()
+        x,y = x.to(device=device),y.to(device=device)
+        # with torch.autocast(device_type=device,dtype=torch.bfloat16):
+        #     logits ,loss = model(x,y)
+        logits ,loss = model(x,y)
+        assert type(loss) ==  torch.Tensor
+        loss = loss / grad_accum_steps #ensure accum loss equal to origin loss
+        loss_accum += loss.detach()
+        loss.backward()
 
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(),1.0)
+    lr = get_lr(step)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+    optimizer.step()
+    torch.mps.synchronize()
+    t1 = time.time()
+    dt = (t1 - t0) * 1000
+    print(f"step{step},loss:{loss_accum:.3f},lr:{lr:.6f},norm:{norm:.4f},dt:{dt:.2f}ms")
 
-
-# while x.size(1) < max_length:
-#     pass
-
-
+import sys; sys.exit(0)
